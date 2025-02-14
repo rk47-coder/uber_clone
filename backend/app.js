4ec69b58382a45144c801f3e2903b9aa3579617a
Raju kumar <rajukumar150198@gmail.com>
2025-02-14 08:17:59 +0530

    make backend sever

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors'); 
+
+
+app.get('/',(req, res) =>{ 
+    res.send ('hello world');
+});
+
+module.exports = app;
